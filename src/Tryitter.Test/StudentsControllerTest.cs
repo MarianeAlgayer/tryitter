063185d70143e0f15a2e1b8d974e3b048e6fcfe2
Mariane Algayer <marianealgayer@gmail.com>
2022-12-12 20:03:51 -0300

    Testes Students Controller

--- src/Tryitter.Test/StudentsControllerTest.cs
+++ src/Tryitter.Test/StudentsControllerTest.cs
@@ -3,6 +3,8 @@ using Tryitter.Context;
 using Tryitter.Models;
 using Tryitter.Controllers;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
 
 namespace Tryitter.Test;
 
@@ -10,7 +12,7 @@ public class StudentsControllerTest
 {
     private readonly AppDbContext _context;
     public static DbContextOptions<AppDbContext> dbContextOptions {get; }
-    public static string connectionString = "Server=localhost;DataBase=TryitterDB;Uid=root;Pwd=laza229955";
+    public static string connectionString = "Server=localhost;DataBase=TryitterDB;Uid=root;Pwd=M@rianeR00T";
 
     static StudentsControllerTest()
     {
@@ -26,7 +28,7 @@ public class StudentsControllerTest
     }
 
     [Fact]
-    public void GetStudentAccounts_Return_OkResult()
+    public void GetStudents_Return_OkResult()
     {
         //Arrange
         var controller = new StudentsController(_context);
@@ -35,6 +37,62 @@ public class StudentsControllerTest
         var data = controller.GetStudents();
 
         //Assert
-        Assert.IsType<IEnumerable<StudentAccount>>(data.Value);
+        Assert.IsType<OkObjectResult>(data.Result);
+    }
+
+    [Fact]
+    public void GetStudent_Return_OkResult()
+    {
+        //Arrange
+        var controller = new StudentsController(_context);
+        var studentId = 1;
+
+        //Act
+        var data = controller.GetStudent(studentId);
+
+        //Assert
+        Assert.IsType<OkObjectResult>(data.Result);
+    }
+
+    [Fact]
+    public void GetStudent_Return_NotFound()
+    {
+        //Arrange
+        var controller = new StudentsController(_context);
+        var studentId = 1000;
+
+        //Act
+        var data = controller.GetStudent(studentId);
+
+        //Assert
+        Assert.IsType<NotFoundObjectResult>(data.Result);
+    }
+
+    [Fact]
+    public void PostStudent_Return_CreatedResult()
+    {
+        //Arrange
+        var controller = new StudentsController(_context);
+        var newStudent = new StudentAccount() { Name = "Mariane Algayer", Email = "mariane.algayer@xpi.com.br", Password = "123456", Status = "Trabalhando", Module = 4 };
+
+        //Act
+        var data = controller.PostStudent(newStudent);
+
+        //Assert
+        Assert.IsType<CreatedAtRouteResult>(data);
+    }
+
+    [Fact]
+    public void DeleteStudent_Return_OkResult()
+    {
+        //Arrange
+        var controller = new StudentsController(_context);
+        var studentId = 1;
+
+        //Act
+        var data = controller.DeleteStudent(studentId);
+
+        //Assert
+        Assert.IsType<OkObjectResult>(data);
     }
 }
