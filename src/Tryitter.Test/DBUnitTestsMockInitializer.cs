cde4bbfdfdc1709d170f5d84a4ada5672012bd59
Mariane Algayer <marianealgayer@gmail.com>
2022-12-12 18:05:12 -0300

    Continuação testes

---
+++ src/Tryitter.Test/DBUnitTestsMockInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Tryitter.Context;
+using Tryitter.Models;
+
+namespace Tryitter.Test
+{
+    public class DBUnitTestsMockInitializer
+    {
+        public DBUnitTestsMockInitializer()
+        { }
+        public void Seed(AppDbContext context)
+        {
+            context.StudentAccounts.Add
+            (new StudentAccount { Name = "Byanca Knorst", Email = "byanca.knorst@xpi.com.br", Password = "123456", Status = "Trabalhando", Module = 4 });
+
+            context.StudentAccounts.Add
+            (new StudentAccount { Name = "Lázaro Kabib", Email = "lazaro.kabib@xpi.com.br", Password = "123456", Status = "Trabalhando", Module = 4 });
+
+            context.StudentAccounts.Add
+            (new StudentAccount { Name = "Mariane Algayer", Email = "mariane.algayer@xpi.com.br", Password = "123456", Status = "Trabalhando", Module = 4 });
+
+            context.SaveChanges();
+        }
+
+    }
+}
---
+++ src/Tryitter.Test/StudentsControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using Auth.Models;
+using Tryitter.Context;
+
+namespace Tryitter.Test;
+
+public class StudentsControllerTest
+{
+    private readonly AppDbContext _context;
+    public StudentsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        var login = new Login();
+    }
+}
